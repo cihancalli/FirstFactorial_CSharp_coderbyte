3e6eabfe83417d22042bebf6ac5142f07ff25249
Cihan ÇALLI <cihan.callii@gmail.com>
2022-03-31 13:31:24 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FirstFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // keep this function call here
+            Console.WriteLine(FirstFactorial(Convert.ToInt32(Console.ReadLine())));
+        }
+
+        public static int FirstFactorial(int num) {
+
+            // code goes here
+            if(num != 1){
+            return num * FirstFactorial(num-1);
+            }
+            return 1;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# First Factorial
+## Big-O notation O(n)
+
+* All the test case outputs were correct
+
+* This user submitted their solution in 1 minutes
